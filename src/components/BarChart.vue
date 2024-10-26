9a6931e575860f41d6a9fcb9a1f8171afbe81446
Svitlana Kostohryz <142225792+SvKost@users.noreply.github.com>
2024-10-26 12:58:40 +0300

    Chart component created

---
+++ src/components/BarChart.vue
@@ -0,0 +1,59 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'
+import { useTransactionsStore } from '../stores/useTransactionsTableStore'
+import { computed } from 'vue'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  setup() {
+    const store = useTransactionsStore()
+
+    const chartData = computed(() => {
+      const transactions = store.topTransactions.slice(0, 10)
+
+      return {
+        labels: transactions.map(
+          t => `${t.payer_name} (${t.payer_edrpou || 'Not found'})`,
+        ),
+        datasets: [
+          {
+            label: 'Сума транзакції',
+            data: transactions.map(t => t.amount),
+            backgroundColor: 'rgba(75, 192, 192, 0.6)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      }
+    })
+
+    const chartOptions = {
+      responsive: true,
+      plugins: {
+        legend: { display: true },
+        tooltip: { enabled: true },
+      },
+    }
+    return {
+      chartData,
+      chartOptions,
+    }
+  },
+}
+</script>
+
+<template>
+  <Bar id="my-chart-id" :data="chartData" :options="chartOptions" />
+</template>
